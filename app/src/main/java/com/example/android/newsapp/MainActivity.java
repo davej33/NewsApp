71826f7f6e649f67bb17bf76f751f70eb7e53711
David Justen <davejusten03@yahoo.com>
2016-11-08 20:34:34 -0500

    returns search query

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // global EditText obj
+    public static EditText search_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set EditText view to obj
+        search_input = (EditText) findViewById(R.id.search_input);
+
+        // set Button view and onClickListener to obj
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search_input.getText();
+                Intent results = new Intent(MainActivity.this, ArticleList.class);
+                startActivity(results);
+            }
+        });
+
     }
 }
